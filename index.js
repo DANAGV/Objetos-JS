9cfd342a7ffc7b4db1dbe76f85c659d943baf214
Arianaomi <arianaomi.lp@gmail.com>
2020-10-02 19:56:04 -0500

    :tada: Initial commit
    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+
+/*
+    crear una función que pida al usuario un año (2000, 1989, etc).
+    la función debe extraer los autos del array cars, cuyo año sea mayor al indicado por el usuario
+    por cada auto extraído, mostrar al usuario un mensaje que diga "Opción ${indice}: ${fabricante del auto} ${nombre del auto} del año ${año de fabricación}, con ${hp} caballos de fuerza"
+*/
+
+const cars = [
+  {
+    year: '2000',
+    maker: 'Toyota',
+    model: 'Corolla',
+    hp: '300'
+  },
+  {
+    year: '1967',
+    maker: 'Ford',
+    model: 'Mustang',
+    hp: '500'
+  },
+  {
+    year: '2000',
+    maker: 'Nissan',
+    model: 'Sentra',
+    hp: '128'
+  },
+  {
+    year: '2012',
+    maker: 'Mitubishi',
+    model: 'Lancer',
+    hp: '300'
+  },
+  {
+    year: '2000',
+    maker: 'Nissan',
+    model: 'Altima',
+    hp: '200'
+  },
+  {
+    year: '1990',
+    maker: 'Volkswagen',
+    model: 'Sedan',
+    hp: '80'
+  },
+  {
+    year: '2015',
+    maker: 'Nissan',
+    model: 'Tida',
+    hp: '200'
+  }
+]
